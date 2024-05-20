4f303132334858e7040682532a4c8ffa332c1740
rin0700 <135789382+rin0700@users.noreply.github.com>
2024-05-20 22:47:13 +0900

    station5完了

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,30 @@
 // DO NOT DELETE
 
+import { useState } from 'react'
 import './App.css'
 
 /**
  * @type {() => JSX.Element}
+ *
  */
+
+
 export const App = () => {
+
+  const [dogUrl, setDogUrl] = useState('https://images.dog.ceo/breeds/spitz-japanese/tofu.jpg')
+  
+  const dogButtonClick = () => {
+    setDogUrl('https://images.dog.ceo/breeds/mastiff-english/1.jpg')
+  }
+
   return (
     <div>
       <header>
-        <h1>LTime</h1>
+        <h1>API</h1>
       </header>
+      <p>私は犬アレルギーです。ぴえん。</p>
+      <button type="button" onClick={dogButtonClick}>更新</button><br/>
+      <img src= {dogUrl} alt="Cute dog"></img>
     </div>
   )
 }
